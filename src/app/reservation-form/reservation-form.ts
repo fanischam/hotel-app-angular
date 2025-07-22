5ed07aec6961d5f05f73f618b0feb024536a8454
Fanis Chamalidis <fchamalidis@yahoo.com>
2025-07-22 19:08:49 +0300

    created initial form structure with validations

--- src/app/reservation-form/reservation-form.ts
+++ src/app/reservation-form/reservation-form.ts
@@ -1,11 +1,59 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-reservation-form',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './reservation-form.html',
-  styleUrl: './reservation-form.css'
+  styleUrl: './reservation-form.css',
 })
-export class ReservationForm {
+export class ReservationForm implements OnInit {
+  reservationForm: FormGroup = new FormGroup({});
 
+  constructor(private formbuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.reservationForm = this.formbuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required],
+    });
+  }
+
+  get checkInDate() {
+    return this.reservationForm.get('checkInDate') as FormControl;
+  }
+
+  get checkOutDate() {
+    return this.reservationForm.get('checkOutDate') as FormControl;
+  }
+
+  get guestName() {
+    return this.reservationForm.get('guestName') as FormControl;
+  }
+
+  get email() {
+    return this.reservationForm.get('email') as FormControl;
+  }
+
+  get roomNumber() {
+    return this.reservationForm.get('roomNumber') as FormControl;
+  }
+
+  onSubmit() {
+    if (this.reservationForm.valid) {
+      // Handle form submission logic here
+      console.log('Form Submitted!');
+    }
+  }
 }
